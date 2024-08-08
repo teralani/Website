6cdcefd57167df8c3193a8410b0d9b50d8712101
teralani <aniketh.terala@gmail.com>
2024-08-08 11:24:47 -0700

    Still need to do the 2 button controls

--- actual/app.js
+++ actual/app.js
@@ -34,7 +34,7 @@ const observer2 = new IntersectionObserver((entries) => {
 
 let options3 = {
     root: null,
-    rootMargin: "0% 0% 0% 0%",
+    rootMargin: "-30% 0% 0% 0%",
     threshold: 0.25,
 }
 
@@ -43,23 +43,23 @@ const observer3 = new IntersectionObserver((entries) => {
         console.log(entry)
         if (entry.isIntersecting) {
             console.log('controls seen');
-            document.querySelectorAll('.controls-container').forEach((el) => el.classList.remove('closed'));
-            document.querySelectorAll('.outer').forEach((el) => el.classList.remove('invisible'));
+            entry.target.querySelectorAll('.controls-container').forEach((el) => el.classList.remove('closed'));
+            entry.target.querySelectorAll('.outer').forEach((el) => el.classList.remove('invisible'));
             
 
-            document.querySelectorAll('.controls-container').forEach((el) => el.classList.add('controls-container-seen'));
-            document.querySelectorAll('.dot').forEach((el) => el.classList.add('dot-seen'));
-            document.querySelectorAll('.blue-thing').forEach((el) => el.classList.add('blue-thing-seen'));
+            entry.target.querySelectorAll('.controls-container').forEach((el) => el.classList.add('controls-container-seen'));
+            entry.target.querySelectorAll('.dot').forEach((el) => el.classList.add('dot-seen'));
+            entry.target.querySelectorAll('.blue-thing').forEach((el) => el.classList.add('blue-thing-seen'));
         } else {
             console.log('controls seen');
-            document.querySelectorAll('.outer').forEach((el) => el.classList.add('invisible'));
+            entry.target.querySelectorAll('.outer').forEach((el) => el.classList.add('invisible'));
 
-            document.querySelectorAll('.controls-container').forEach((el) => el.classList.remove('controls-container-seen'));
-            document.querySelectorAll('.controls-container').forEach((el) => el.classList.add('closed'));
+            entry.target.querySelectorAll('.controls-container').forEach((el) => el.classList.remove('controls-container-seen'));
+            entry.target.querySelectorAll('.controls-container').forEach((el) => el.classList.add('closed'));
             
-            document.querySelectorAll('.dot').forEach((el) => el.classList.remove('dot-seen'));
+            entry.target.querySelectorAll('.dot').forEach((el) => el.classList.remove('dot-seen'));
             
-            document.querySelectorAll('.blue-thing').forEach((el) => el.classList.remove('blue-thing-seen'));
+            entry.target.querySelectorAll('.blue-thing').forEach((el) => el.classList.remove('blue-thing-seen'));
         }
         });
 }, options3);
@@ -74,22 +74,42 @@ const observer4 = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
-            document.querySelectorAll('.outer').forEach((el) => el.style.position = "relative");
+            entry.target.querySelectorAll('.outer').forEach((el) => el.style.position = "relative");
         } else {
-            document.querySelectorAll('.outer').forEach((el) => el.style.position = "fixed");
+            entry.target.querySelectorAll('.outer').forEach((el) => el.style.position = "fixed");
         }
         });
 }, options4);
 
+let options5 = {
+    root: null,
+    rootMargin: "0% 0% 0% 0%",
+    threshold: 0.60,
+}
+
+const observer5 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            document.getElementById('sticky-navbar').classList.remove('seen');
+        } else {
+            document.getElementById('sticky-navbar').classList.add('seen');
+        }
+        });
+}, options5);
+
 const header1s = document.querySelectorAll('.hidden1');
 const header2s = document.querySelectorAll('.hidden2');
-const controls = document.querySelectorAll('.slider');
+const slider = document.querySelectorAll('.slider');
 header1s.forEach((el) => observer1.observe(el));
 header2s.forEach((el) => observer2.observe(el));
-controls.forEach((el) => observer3.observe(el));
+slider.forEach((el) => observer3.observe(el));
 
 const controls_placeholder = document.querySelectorAll('.outer-controls-placeholder');
 controls_placeholder.forEach((el) => observer4.observe(el));
+
+const navbar = document.querySelectorAll('#section1');
+navbar.forEach((el) => observer5.observe(el));
 
 function setUpVideo() {
     var video = document.getElementById('video')
@@ -105,12 +125,12 @@ function control1(button) {
     // var checked = document.querySelector(".slider").querySelector('input[name="selector"]:checked').value;
     var s = document.getElementsByClassName('slider__inner')[0];
     console.log("selected: " + button.value);
-    if(button.value == "slide1") {s.style.left = '7vw'}
-    if(button.value  == "slide2") {s.style.left = '-76vw'}
-    if(button.value  == "slide3") {s.style.left = '-158.5vw'}
-    if(button.value  == "slide4") {s.style.left = '-241vw'}
-    if(button.value  == "slide5") {s.style.left = '-324vw'}
-    if(button.value  == "slide6") {s.style.left = '-406.5vw'}
+    if(button.value == "slide1") {s.style.left = '23vw'}
+    if(button.value  == "slide2") {s.style.left = '-26vw'}
+    if(button.value  == "slide3") {s.style.left = '-75vw'}
+    if(button.value  == "slide4") {s.style.left = '-124vw'}
+    if(button.value  == "slide5") {s.style.left = '-173vw'}
+    if(button.value  == "slide6") {s.style.left = '-222vw'}
     }
 }
 
@@ -118,3 +138,10 @@ let slider1_buttons = document.querySelector(".slider").querySelectorAll('input[
 slider1_buttons.forEach(rb=>rb.addEventListener("change", e => {
     control1(rb)
 }));
+let nav = document.getElementById("navbar");
+nav.addEventListener("mouseenter", e=> {
+    document.getElementById("blurred").style.display = "block";
+});
+nav.addEventListener("mouseleave", e=> {
+    document.getElementById("blurred").style.display = "none";
+});
