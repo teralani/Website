cb532b321686983fb268b3e266688432949f3ec5
teralani <aniketh.terala@gmail.com>
2024-08-13 23:56:55 -0700

    Almost done!!

--- actual/app.js
+++ actual/app.js
@@ -1,3 +1,7 @@
+function clamp(number, min, max) {
+    return Math.max(min, Math.min(number, max));
+}
+
 let options1 = {
     root: null,
     rootMargin: "0% 0% -20% 0%",
@@ -6,7 +10,7 @@ let options1 = {
 
 const observer1 = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        // console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('in-view1');
         } else {
@@ -23,7 +27,7 @@ let options2 = {
 
 const observer2 = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        // console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('in-view2');
         } else {
@@ -40,7 +44,7 @@ let options3 = {
 
 const observer3 = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        // console.log(entry)
         if (entry.isIntersecting) {
             console.log('controls seen');
             entry.target.querySelectorAll('.controls-container').forEach((el) => el.classList.remove('closed'));
@@ -49,6 +53,7 @@ const observer3 = new IntersectionObserver((entries) => {
 
             entry.target.querySelectorAll('.controls-container').forEach((el) => el.classList.add('controls-container-seen'));
             entry.target.querySelectorAll('.dot').forEach((el) => el.classList.add('dot-seen'));
+            entry.target.querySelectorAll('.dot-two-button-highlight').forEach((el) => el.classList.add('dot-seen'));
             entry.target.querySelectorAll('.blue-thing').forEach((el) => el.classList.add('blue-thing-seen'));
         } else {
             console.log('controls seen');
@@ -58,6 +63,7 @@ const observer3 = new IntersectionObserver((entries) => {
             entry.target.querySelectorAll('.controls-container').forEach((el) => el.classList.add('closed'));
             
             entry.target.querySelectorAll('.dot').forEach((el) => el.classList.remove('dot-seen'));
+            entry.target.querySelectorAll('.dot-two-button-highlight').forEach((el) => el.classList.add('dot-seen'));
             
             entry.target.querySelectorAll('.blue-thing').forEach((el) => el.classList.remove('blue-thing-seen'));
         }
@@ -72,7 +78,7 @@ let options4 = {
 
 const observer4 = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        // console.log(entry)
         if (entry.isIntersecting) {
             entry.target.querySelectorAll('.outer').forEach((el) => el.style.position = "relative");
         } else {
@@ -89,7 +95,7 @@ let options5 = {
 
 const observer5 = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        // console.log(entry)
         if (entry.isIntersecting) {
             document.getElementById('sticky-navbar').classList.remove('seen');
         } else {
@@ -101,9 +107,48 @@ const observer5 = new IntersectionObserver((entries) => {
 const header1s = document.querySelectorAll('.hidden1');
 const header2s = document.querySelectorAll('.hidden2');
 const slider = document.querySelectorAll('.slider');
+
 header1s.forEach((el) => observer1.observe(el));
 header2s.forEach((el) => observer2.observe(el));
 slider.forEach((el) => observer3.observe(el));
+
+let hoptions = {
+    root: null,
+    rootMargin: "0% -50% 0% 0%",
+    threshold: 0.15,
+}
+
+const info = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        // console.log(entry)
+        if (entry.isIntersecting) {
+            // console.log('info seen');
+
+            entry.target.nextElementSibling.querySelectorAll('.controls-container').forEach((el) => el.classList.remove('closed'));
+            entry.target.nextElementSibling.querySelectorAll('.outer').forEach((el) => el.classList.remove('invisible'));
+            
+
+            entry.target.nextElementSibling.querySelectorAll('.controls-container').forEach((el) => el.classList.add('controls-container-seen'));
+            entry.target.nextElementSibling.querySelectorAll('h2').forEach((el) => el.classList.add('dot-seen'));
+            entry.target.nextElementSibling.querySelectorAll('.blue-thing').forEach((el) => el.classList.add('blue-thing-seen'));
+        } else {
+            // console.log('info gone');
+            entry.target.nextElementSibling.querySelectorAll('.outer').forEach((el) => el.classList.add('invisible'));
+
+            entry.target.nextElementSibling.querySelectorAll('.controls-container').forEach((el) => el.classList.remove('controls-container-seen'));
+            entry.target.nextElementSibling.querySelectorAll('.controls-container').forEach((el) => el.classList.add('closed'));
+            
+            entry.target.nextElementSibling.querySelectorAll('h2').forEach((el) => el.classList.remove('dot-seen'));
+            
+            entry.target.nextElementSibling.querySelectorAll('.blue-thing').forEach((el) => el.classList.remove('blue-thing-seen'));
+        }
+        });
+}, hoptions);
+
+const s3_headers = document.querySelector('#section3').querySelectorAll('.block');
+s3_headers.forEach((el) => info.observe(el));
+
+
 
 const controls_placeholder = document.querySelectorAll('.outer-controls-placeholder');
 controls_placeholder.forEach((el) => observer4.observe(el));
@@ -120,7 +165,7 @@ function setUpVideo() {
 }
 
 function control1(button) {
-    console.log(button.value);
+    // console.log(button.value);
     if(button.checked == true) {
     // var checked = document.querySelector(".slider").querySelector('input[name="selector"]:checked').value;
     var s = document.getElementsByClassName('slider__inner')[0];
@@ -154,7 +199,7 @@ nav.addEventListener("mouseleave", e=> {
 });
 
 function control2(button) {
-    console.log("selected: " + button.value);
+    // console.log("selected: " + button.value);
     var one = document.querySelectorAll('.slider')[1].querySelectorAll(".slider__card")[0];
     var one1 = document.querySelectorAll(".grey")[0];
     var two = document.querySelectorAll('.slider')[1].querySelectorAll(".slider__card")[1];
@@ -181,7 +226,72 @@ function control2(button) {
     }
 };
 let slider2_buttons = document.querySelectorAll('input[name="selector2"]');
-console.log(slider2_buttons);
+// console.log(slider2_buttons);
 slider2_buttons.forEach(rb=>rb.addEventListener("change", e => {
     control2(rb)
 }));
+
+
+// const videoObserver = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry);
+//         if(entry.isIntersecting) {
+//             entry.target.play();
+//         } else {
+//             entry.target.pause();
+//         }
+//     });
+// }, videoOptions);
+
+// let videoOptions = {
+//     root: null,
+//     rootMargin: "0% 0% 0% 0%",
+//     threshold: 0.2,
+// }
+
+// const thin_video = document.querySelector("#section3").querySelectorAll(".videos")
+// thin_video.forEach(el => videoObserver.observe(el))
+
+
+var vid_offset = 9999999;
+// window.addEventListener("scroll", function(){
+//     var s =  window.scrollY
+//     clip.style.transform = "matrix(" + "" + ", 0, 0," + "" + ", 0, 0)";
+// })
+
+let vid = document.querySelector(".moveable");
+let vid_options = {root: null, rootMargin: "0% 0% 0% 0%", threshold: 0.9};
+
+const videoObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+
+            if(window.scrollY <= vid_offset) {
+                vid_offset = window.scrollY;
+            }
+            // // document.querySelector("#clip").style.display = "block";
+            // document.querySelector("#clip").style.position = "sticky";
+
+            console.log(entry.intersectionRatio);
+            
+            window.addEventListener("scroll", function(){
+                var clip = document.querySelector("#clip");
+                var calculations = clamp(2 **(9 - 10*(window.scrollY - vid_offset)/(vid.offsetHeight)), 1.5, 500);
+                clip.style.transform = "matrix(" + calculations + ", 0, 0," + calculations + ", 0, 0)";
+            });
+            
+        }
+    })
+}, vid_options);
+
+videoObserver.observe(vid);
+
+function vidbutton(el) {
+    console.log(el);
+    console.log(el.parentElement.nextElementSibling.nextElementSibling.nodeName);
+    if(el.checked) {
+        el.parentElement.nextElementSibling.nextElementSibling.play();
+    } else {
+        el.parentElement.nextElementSibling.nextElementSibling.pause();
+    }
+}
