cd17bdd7f9cee26997ffd52d32246aa845b75e33
teralani <aniketh.terala@gmail.com>
2024-08-14 15:41:29 -0700

    basically done

--- actual/app.js
+++ actual/app.js
@@ -168,7 +168,10 @@ function control1(button) {
     // console.log(button.value);
     if(button.checked == true) {
     // var checked = document.querySelector(".slider").querySelector('input[name="selector"]:checked').value;
-    var s = document.getElementsByClassName('slider__inner')[0];
+    var s = document.getElementsByClassName('slider__wrapper')[0];
+    var o = (s.scrollWidth - s.clientWidth)/5;
+        console.log(o)
+
     console.log("selected: " + button.value);
     // if(button.value == "slide1") {s.style.left = '23vw'}
     // if(button.value  == "slide2") {s.style.left = '-26vw'}
@@ -177,12 +180,12 @@ function control1(button) {
     // if(button.value  == "slide5") {s.style.left = '-173vw'}
     // if(button.value  == "slide6") {s.style.left = '-222vw'}
 
-    if(button.value == "slide1") {s.style.left = '0'}
-    if(button.value  == "slide2") {s.style.left = '-55%'}
-    if(button.value  == "slide3") {s.style.left = '-107%'}
-    if(button.value  == "slide4") {s.style.left = '-160%'}
-    if(button.value  == "slide5") {s.style.left = '-215%'}
-    if(button.value  == "slide6") {s.style.left = '-265%'}
+    if(button.value == "slide1") {s.scroll(0, 0)}
+    if(button.value  == "slide2") {s.scroll(o*1, 0)}
+    if(button.value  == "slide3") {s.scroll(o*2, 0)}
+    if(button.value  == "slide4") {s.scroll(o*3, 0)}
+    if(button.value  == "slide5") {s.scroll(o*4, 0)}
+    if(button.value  == "slide6") {s.scroll(o*5, 0)}
     }
 }
 
@@ -190,7 +193,7 @@ let slider1_buttons = document.querySelector(".slider").querySelectorAll('input[
 slider1_buttons.forEach(rb=>rb.addEventListener("change", e => {
     control1(rb)
 }));
-let nav = document.getElementById("navbar");
+let nav = document.querySelector(".nav-list");
 nav.addEventListener("mouseenter", e=> {
     document.getElementById("blurred").style.display = "block";
 });
